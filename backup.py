12b4763c3ed9fe41bc5074196d564a025a4d53ba
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-02 21:30:54 -0500

    add try and except block

--- backup.py
+++ backup.py
@@ -7,3 +7,12 @@ import time
 source_dir =
 destination_dir =
 
+def copy_folder_to_directory(source, dest):
+  today = datetime.date.today()
+  dest_dir = os.path.join(dest, str(today))
+
+  try:
+    shutil.copytree(source, dest_dir)
+    print(f'Folder copied to: {dest_dir}')
+  except FileExistsError:
+    print(f'Folder already exists in : {dest}')
